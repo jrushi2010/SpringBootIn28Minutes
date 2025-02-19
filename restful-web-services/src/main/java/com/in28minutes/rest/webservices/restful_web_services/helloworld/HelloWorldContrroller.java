32d77f364aaec63d23ddb5cedb92070d32be51cd
jrushi2010 <jrushi2010@gmail.com>
2025-02-19 11:49:00 +0530

    Hello world rest api with a path variable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContrroller.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContrroller.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class HelloWorldContrroller {
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean HelloWorldBean() {
 		return new HelloWorldBean("Hello World");
+	}
+	
+	@GetMapping(path = "/hello-world/Path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
 	
