8f05d74fb36ecdcb575368fd906e0af18536a9a7
jrushi2010 <jrushi2010@gmail.com>
2025-02-19 17:38:41 +0530

    implemented our own custom exception structure, so we can return right response and the response status.

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.in28minutes.rest.webservices.restful_web_services.exception;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.in28minutes.rest.webservices.restful_web_services.user.UserNotFoundException;
+
+@ControllerAdvice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<ErrorDetails> handleAllException(Exception ex,WebRequest request) throws Exception {
+		//we are using our own custom exception structure and we are return it as the response
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+				ex.getMessage(),request.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(errorDetails,HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex,WebRequest request) throws Exception {
+		//we are using our own custom exception structure and we are return it as the response
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+				ex.getMessage(),request.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(errorDetails,HttpStatus.NOT_FOUND);
+		
+	}
+	
+	
+}
