343508f8dd7b7691ebad2c6dc052a77310d9b8cb
jrushi2010 <jrushi2010@gmail.com>
2025-02-19 17:05:06 +0530

    Enhanced the post request so it will return correct HTTP Status code and it will return the correct URL as a response in the header.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -28,7 +31,17 @@ public class UserResource {
 	}
 	
 	@PostMapping("users")
-	public void createUser(@RequestBody User user) {
-		service.save(user);
+	public ResponseEntity<User> createUser(@RequestBody User user) {
+		User savedUser = service.save(user);
+		
+		//sending created user as a response in header
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}") //so we are appending id on the current request and replacing with the value
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		return ResponseEntity.created(location ).build();
 	}
+	
+	
+	
 }
