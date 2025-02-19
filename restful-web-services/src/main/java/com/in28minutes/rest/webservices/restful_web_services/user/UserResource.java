c3bc7308316352048195fe6cfac08bcd992edcc7
jrushi2010 <jrushi2010@gmail.com>
2025-02-19 12:19:48 +0530

    implemented get user to retrieve single user.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -3,6 +3,7 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +18,10 @@ public class UserResource {
 	@GetMapping(path = "/users")
 	public List<User> retrieveAllUsers(){
 		return service.findAll();
+	}
+	
+	@GetMapping(path = "/users/{id}")
+	public User retrieveUser(@PathVariable int id){
+		return service.findOne(id);
 	}
 }
