f646900ec0f57b2ff0159975a87ee546d1b12a42
jrushi2010 <jrushi2010@gmail.com>
2025-02-20 10:41:50 +0530

    Implemented delete User method so we can delete the user.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,6 +48,8 @@ public class UserResource {
 		return ResponseEntity.created(location ).build();
 	}
 	
-	
-	
+	@DeleteMapping(path = "/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		service.deleteById(id);
+	}
 }
