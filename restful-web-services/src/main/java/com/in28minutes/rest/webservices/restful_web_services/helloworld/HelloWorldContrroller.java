ec4c54e43c5c7489854041ad3b9406944d18994c
jrushi2010 <jrushi2010@gmail.com>
2025-02-21 21:51:17 +0530

    Exploring Internationalization for Rest API using Header Accept-Language and value is nl for Dutch.

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContrroller.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloworld/HelloWorldContrroller.java
@@ -1,11 +1,21 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldContrroller {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldContrroller(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 
 	
 	@GetMapping(path = "/hello-world")
@@ -23,5 +33,18 @@ public class HelloWorldContrroller {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String HelloWorldInternationlised() {
+		
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		
+		
+		
+		//Example : en - English (Good Morning)
+		//Example : nl - Dutch (Goedemorgen)
+		//Example : fr - French (Bonjour)
+		//Example : de - Deutsch (Guten Morgen)
+	}
 	
 }
