2572f7df0a0c60f05c1b7dc3e27c50cb98135390
jrushi2010 <jrushi2010@gmail.com>
2025-02-21 22:13:25 +0530

    Versioning Rest API - using Request Param and Headers

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/versioning/VersioningPersonController.java
@@ -17,4 +17,23 @@ public class VersioningPersonController {
 		return new PersonV2(new Name("Bob","Charlie"));
 	}
 	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getFirstVersionOfPersonRequestParameter() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getSecondVersionOfPersonRequestParamter() {
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getFirstVersionOfPersonRequestHeader() {
+		return new PersonV1("Bob Charlie");
+	}
+	
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getSecondVersionOfPersonRequestHeader() {
+		return new PersonV2(new Name("Bob","Charlie"));
+	}
 }
